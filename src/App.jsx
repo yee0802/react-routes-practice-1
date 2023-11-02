3a29fd46fbe2b55e690b9ab5568bf44187b19c8b
Julian Ng <julesnuggy@gmail.com>
2023-11-02 15:48:13 +0000

    Extract data to own file

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,9 @@
-import { useState } from "react";
-import { Route, Routes, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import ProductsPage from "./pages/Products";
-import "./App.css";
-
-const productsData = [
-  {
-    id: 1,
-    name: "iMac",
-    type: "computer",
-    price: 1400,
-    inventory: {
-      quantity: 100,
-      incomingDelivery: false
-    }
-  },
-  {
-    id: 2,
-    name: "Macbook Pro",
-    type: "computer",
-    price: 2000,
-    inventory: {
-      quantity: 10,
-      incomingDelivery: true
-    }
-  },
-  {
-    id: 3,
-    name: "iPad Mini",
-    type: "tablet",
-    price: 600,
-    inventory: {
-      quantity: 200,
-      incomingDelivery: false
-    }
-  },
-  {
-    id: 4,
-    name: "iPad Pro",
-    type: "tablet",
-    price: 800,
-    inventory: {
-      quantity: 0,
-      incomingDelivery: true
-    }
-  }
-];
-
-const appsData = [
-  {
-    id: 1,
-    name: "Farmville",
-    type: "game",
-    price: 0
-  },
-  {
-    id: 2,
-    name: "Facebook",
-    type: "social",
-    price: 0
-  }
-];
+import { useState } from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
+import { appsData, productsData } from './data.js';
+import Home from './pages/Home';
+import ProductsPage from './pages/Products';
+import './App.css';
 
 export default function App() {
   const [products, setProducts] = useState(productsData);
